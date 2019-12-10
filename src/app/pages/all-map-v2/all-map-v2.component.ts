d53900f60d897244e97d0375ceca617de6ebf5fc
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-12-10 15:07:04 +0700

    init alert.service

--- src/app/pages/all-map-v2/all-map-v2.component.ts
+++ src/app/pages/all-map-v2/all-map-v2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, Map, marker, layerGroup, icon, featureGroup } from 'leaflet';
+
+import { AlertService, Device } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-all-map-v2',
@@ -7,14 +9,34 @@ import { tileLayer, latLng } from 'leaflet';
   styleUrls: ['./all-map-v2.component.scss']
 })
 export class AllMapV2Component implements OnInit {
-  options = {
-    layers: tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-    zoom: 13,
-    center: latLng(-6.8794222, 107.5386812)
+  options = { layers: tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png') };
+  map: Map;
+  markerIcon = {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40],
+      iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
+      shadowUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png"
+    })
   };
+  dangers: Device[];
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.dangers = this.alertService.getDanger();
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+    const markers = this.dangers.map(danger => marker(latLng(danger.position.lat, danger.position.lng), this.markerIcon)),
+      layer = featureGroup(markers);
+    this.map.addLayer(layer);
+    this.map.fitBounds(layer.getBounds());
+  }
+
+  focusTo(event, device: Device) {
+    this.map.panTo(latLng(device.position.lat, device.position.lng));
   }
 }
