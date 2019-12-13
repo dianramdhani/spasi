03f58079da1e949ed70bca7c5603f1171c2f291c
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-12-13 20:29:50 +0700

    update all-map-v2: fix issue minor

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -10,17 +10,17 @@ export class AlertService {
   getDangers(): Device[] {
     return [
       {
-        position: { lat: -6.8973318, lng: 107.6096949 },
+        position: { lat: -6.917464, lng: 107.619125 },
         site: 'BTSH Bandung 1',
         suggestAction: 'High Temperature'
       },
       {
-        position: { lat: -6.892688, lng: 107.6181923 },
+        position: { lat: -7.357977, lng: 107.195717 },
         site: 'BTSH Cianjur 2',
         suggestAction: 'Low Voltage'
       },
       {
-        position: { lat: -6.880897, lng: 107.6266693 },
+        position: { lat: -7.350581, lng: 108.217163 },
         site: 'IPLH Tasikmalaya',
         suggestAction: 'Empty Fuel Tank'
       }
@@ -30,22 +30,22 @@ export class AlertService {
   getWarnings(): Device[] {
     return [
       {
-        position: { lat: -6.876269, lng: 107.5298403 },
+        position: { lat: 5.548290, lng: 95.323753 },
         site: 'BTSP Banda Aceh 1',
         suggestAction: 'Warning Temperature'
       },
       {
-        position: { lat: -6.875257, lng: 107.5265363 },
+        position: { lat: -5.147665, lng: 119.432732 },
         site: 'PAK Makasar',
         suggestAction: 'Warning Temperature'
       },
       {
-        position: { lat: -6.874969, lng: 107.5252493 },
+        position: { lat: -8.528460, lng: 140.540375 },
         site: 'BTSP Merauke',
         suggestAction: 'Warning Temperature'
       },
       {
-        position: { lat: -6.873467, lng: 107.5204963 },
+        position: { lat: 0.785560, lng: 127.380829 },
         site: 'PAK Ternate',
         suggestAction: 'Warning Temperature'
       }
@@ -55,16 +55,24 @@ export class AlertService {
   getNormals(): Device[] {
     return [
       {
-        position: { lat: -6.880897, lng: 107.6266693 },
-        site: 'Site Normal 1',
+        position: { lat: -6.175110, lng: 106.865036 },
+        site: 'PAK Jakarta',
       },
       {
-        position: { lat: -6.875257, lng: 107.5265363 },
-        site: 'Site Normal 2',
+        position: { lat: -3.625530, lng: 128.105453 },
+        site: 'BTSH Ambon',
       },
       {
-        position: { lat: -6.880897, lng: 107.5204963 },
-        site: 'Site Normal 3',
+        position: { lat: -0.303090, lng: 100.367610 },
+        site: 'BTSH Bukittinggi',
+      },
+      {
+        position: { lat: -8.670458, lng: 115.212631 },
+        site: 'IPLH Denpasar',
+      },
+      {
+        position: { lat: 0.668000, lng: 117.544739 },
+        site: 'PAK Kutai',
       }
     ]
   }
