e4fbee77ac7c005ee90d2eda2ff268c204e30702
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-12-13 17:48:56 +0700

    fix login.guard

--- src/app/services/login.guard.ts
+++ src/app/services/login.guard.ts
@@ -12,11 +12,12 @@ export class LoginGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const storagePrefix = window['config']().storagePrefix,
-      { loginSucces } = JSON.parse(localStorage.getItem(`${storagePrefix}-login`));
-    if (loginSucces) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
+      login = JSON.parse(localStorage.getItem(`${storagePrefix}-login`));
+    if (login) {
+      if (login.loginSucces) {
+        return true;
+      }
     }
+    this.router.navigate(['/login']);
   }
 }
